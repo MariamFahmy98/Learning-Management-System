0eca1144d1449d753ba98dc20e3f8ccef8220801
mostafaalgady <52762475+mostafaalgady@users.noreply.github.com>
2020-12-15 15:15:35 +0200

    Add files via upload

---
+++ LMS_application/lib/Widgets/CustomeTile.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import './Screens/MyCourses.dart';
+import './Screens/Announcements.dart';
+import './Screens/Assignments.dart';
+import './Screens/Grades.dart';
+import './Screens/Quizes.dart';
+
+class CustomeTile extends StatelessWidget {
+  IconData myIcon;
+  String txt;
+  Function tap;
+
+  Map<String, Widget> screens = {
+    'MyCourses': MyCourses(),
+    'Announcements': Announcements(),
+    'Grades': Grades(),
+    'Quizes': Quizes(),
+    'Assignments': Assignments(),
+  };
+  CustomeTile({this.myIcon, this.txt, this.tap});
+
+  void selectScreen(BuildContext ctx) {
+    Navigator.of(ctx).push(
+      MaterialPageRoute(builder: (_) {
+        return screens[txt];
+      }),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
+      child: InkWell(
+        onTap: () => selectScreen(context),
+        splashColor: Colors.purple,
+        child: Container(
+          decoration: BoxDecoration(
+              border: Border(bottom: BorderSide(color: Colors.grey))),
+          height: 50.0,
+          child: Row(
+            children: <Widget>[
+              Icon(myIcon),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  txt,
+                  style: TextStyle(
+                    fontSize: 16.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
