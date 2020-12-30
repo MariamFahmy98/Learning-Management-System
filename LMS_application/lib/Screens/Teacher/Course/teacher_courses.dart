dd67858e034e768059783d7909c4df13a061ab4e
Mustafa El-Antrawy <52972831+MustafaElAntrawy@users.noreply.github.com>
2020-12-30 16:58:07 +0200

    Viewing Teacher courses from Database

--- LMS_application/lib/Screens/Teacher/Course/teacher_courses.dart
+++ LMS_application/lib/Screens/Teacher/Course/teacher_courses.dart
@@ -4,48 +4,9 @@ import 'package:flutter/material.dart';
 import 'teacher_course_list.dart';
 import 'teacher_add_course.dart';
 
-class TeacherCourses extends StatefulWidget {
-  final Teacher teacher;
-  final List<Course> allCoursesAll;
-  TeacherCourses({this.teacher, this.allCoursesAll});
-  @override
-  _TeacherCourses createState() => _TeacherCourses(teacher, allCoursesAll);
-}
-
-class _TeacherCourses extends State<TeacherCourses> {
-  List<Course> allCourses;
+class TeacherCourses extends StatelessWidget {
   Teacher teacher;
-  _TeacherCourses(this.teacher, this.allCourses);
-
-  void addNewCourse(String crName, String crCode, String crDescription,
-      String crCreditHours) {
-//final newCr = Course(crCode);
-    //  allCourses.add(newCr);
-    setState(() {
-      // teacher.courses.add(newCr);
-    });
-  }
-
-  // void _deleteCourse(String code) {
-  //   setState(() {
-  //     teacher.myCourses.removeWhere((course) {
-  //       return course.courseCode == code;
-  //     });
-  //   });
-  // }
-
-  void _startAddNewCourse(BuildContext ctx) {
-    showModalBottomSheet(
-      context: ctx,
-      builder: (_) {
-        return GestureDetector(
-          onTap: () {},
-          child: TeacherAddCourse(addNewCourse),
-          behavior: HitTestBehavior.opaque,
-        );
-      },
-    );
-  }
+  //TeacherCourses(this.teacher);
 
   @override
   Widget build(BuildContext context) {
@@ -55,22 +16,11 @@ class _TeacherCourses extends State<TeacherCourses> {
       ),
       body: SingleChildScrollView(
         child: Column(
-          //mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            TeacherCoursesList(
-                //myCourses: teacher.myCourses,
-                //deleteCourse: _deleteCourse,
-                ),
+            TeacherCoursesList(teacher),
           ],
         ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          _startAddNewCourse(context);
-        },
       ),
     );
   }
